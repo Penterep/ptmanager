b1792dce4bcd784582be10ba6e1498e35ea1d7ad
Daniel <d.kummel@penterep.com>
2025-07-14 22:47:06 +0200

    Refactor burp_listener.py

--- ptmanager/modules/burp_listener.py
+++ ptmanager/modules/burp_listener.py
@@ -5,20 +5,22 @@ import sys
 import os
 import time
 import threading
-from concurrent.futures import ThreadPoolExecutor
+import queue
 
 from ptlibs import ptjsonlib
 
-
 class BurpSocketListener:
-    def __init__(self, port, data_callback=None):
+    def __init__(self, daemon, config, port, data_callback=None):
         print(f"Starting listener on port: {port}")
-        self.data_callback = data_callback
         self.host = '127.0.0.1'  # Localhost
         self.port = port #56651
         self.server_socket = None
         self.client_connection = None
         self.client_address = None
+        self.daemon = daemon
+        self.config = config
+
+        self.domain_guid_map = {}  # domain -> guid
 
         # Start the server socket
         self.start_server_socket()
@@ -28,6 +30,19 @@ class BurpSocketListener:
         self.listen_thread.start()
 
         self.data_callback = data_callback
+
+        self.burpsuite_data_queue = queue.Queue()
+
+        self.processing_thread = threading.Thread(target=self.process_incoming_burpsuite_data, daemon=True)
+        self.processing_thread.start()
+
+    def process_incoming_burpsuite_data(self):
+        """Process incoming BurpSuite data in a dedicated thread."""
+        while True:
+            if not self.domain_guid_map:
+                self.clear_burp_queue()
+            else:
+                self.process_burp_data()
 
     def start_server_socket(self):
         """Start the server socket to accept a single client."""
@@ -63,11 +78,12 @@ class BurpSocketListener:
         """Listen for data from the connected client and print it."""
         while True:
             try:
-                data = self.receive_full_data(self.client_connection)
+                data = self.receive_full_data(self.client_connection) # receive from extension
                 if data:
-                    # Full data. Sends json to shared que
-                    if self.data_callback:
-                        self.data_callback(data)
+                    # Full data. Sends json to que
+                    self.burpsuite_data_queue.put(data)
+                    #if self.data_callback:
+                        #self.data_callback(data)
                 else:
                     print("Client disconnected, attempting to reconnect...")
                     self.client_connection.close()  # Zavře aktuální připojení
@@ -82,32 +98,77 @@ class BurpSocketListener:
         delimiter = "__endS0ck3tMsg__"
 
         while True:
-            chunk = conn.recv(1024).decode('utf-8')
+            chunk = conn.recv(4096)
             if not chunk:
                 return None  # klient ukončil spojení
 
-            buffer += chunk
+            buffer += chunk.decode('utf-8')
 
             while delimiter in buffer:
-                message, buffer = buffer.split(delimiter, 1)
+                raw_message, buffer = buffer.split(delimiter, 1)
+                message = raw_message.strip()
+
                 try:
-                    return json.loads(message.strip())
+                    print("OK", message)
+                    return json.loads(message)
                 except json.JSONDecodeError as e:
-                    print(f"[ERROR] Failed to parse JSON: {e}")
-                    return None
+                    #print(f"[ERROR] Failed to parse JSON: {e}", "MESSAGE:", message)
+                    # Just a fragment, expecting more..
+                    buffer = message + delimiter + buffer
+                    break  # break out of inner while, recv more data
 
     def send_data_to_client(self, data):
         """Send JSON data to the connected client (Burp plugin)."""
         if not self.client_connection:
             print("[WARN] No client connected. Cannot send data.")
             return
-
         try:
             message = json.dumps(data) + "__endS0ck3tMsg__"
             self.client_connection.sendall(message.encode('utf-8'))
             print("[INFO] Sent data to Burp plugin:", data)
         except Exception as e:
             print(f"[ERROR] Failed to send data to client: {e}")
+
+    def send_domain_to_burp_scope(self, domain):
+        try:
+            scope_data = {"action": "add_scope", "domain": domain}
+            self.send_data_to_client(scope_data)
+        except Exception as e:
+            print(f"Error sending scope update to Burp: {e}")
+
+    def clear_burp_queue(self):
+        """Clear BurpSuite queue if domain_guid_map is empty."""
+        while True:
+            try:
+                self.burpsuite_data_queue.get_nowait()
+            except queue.Empty:
+                break
+
+    def process_burp_data(self):
+        """Process BurpSuite data."""
+        try:
+            burp_data = self.burpsuite_data_queue.get_nowait()
+            domain = burp_data.pop("domain", None)
+            guid = self.domain_guid_map.get(domain)
+            self.handle_burp_data_with_guid(burp_data, guid)
+        except queue.Empty:
+            pass
+
+    def handle_burp_data_with_guid(self, data, guid):
+        """Handle Burp data and forward it to the API."""
+        data["satid"] = self.config.get_satid()
+        data["guid"]  = guid
+
+        time.sleep(1)
+
+        response = self.daemon.send_to_api("result-proxy", data)
+
+        try:
+            res_data = response.json()
+            if res_data.get("success"):
+                self.send_data_to_client(res_data.get("data"))
+        except:
+            return
 
 def parse_args():
     parser = argparse.ArgumentParser(usage=f"burp_listener.py <options>")
